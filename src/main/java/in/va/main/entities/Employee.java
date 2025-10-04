aacd5cdabc918d2715f148bd6666b5f55d87afca
Vishal <vishal.ambhore@omunim.com>
2025-10-04 11:58:18 +0530

    fields added

--- src/main/java/in/va/main/entities/Employee.java
+++ src/main/java/in/va/main/entities/Employee.java
@@ -25,6 +25,18 @@ public class Employee {
 	@Column
 	private String password;
 	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	@Column
 	private String city;
 	
